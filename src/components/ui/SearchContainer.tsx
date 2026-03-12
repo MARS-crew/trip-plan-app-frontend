c31a3e539eab29c5adfe7b2c20cd7dbb593a3d25
Kimbosung521 <sorisl37@naver.com>
2026-03-13 05:32:39 +0900

    fix: PLI-91 (위시리스트 바텀시트 중간 저장)

--- src/components/ui/SearchContainer.tsx
+++ src/components/ui/SearchContainer.tsx
@@ -5,18 +5,15 @@ export interface SearchContainerProps extends ViewProps {
   children: React.ReactNode;
 }
 
-export const SearchContainer: React.FC<SearchContainerProps> = ({ children,  ...props }) => {
+export const SearchContainer: React.FC<SearchContainerProps> = ({ children, ...props }) => {
   return (
-    <View 
+    <View
       // absolute 위치와 기본 디자인 레이아웃 설정
-   
-    
-      {...props}
-    >
-      <View 
+
+      {...props}>
+      <View
         // 흰색 배경, 둥근 모서리, 가로 정렬, 그림자 설정
-        className="flex-row items-center bg-white px-4 py-1 border-[1px] rounded-xl  border-borderGray "
-      >
+        className="flex-row items-center bg-white border-[1px] h-14 rounded-xl  border-borderGray ">
         {children}
       </View>
     </View>
--- src/screens/WishlistScreen.tsx
+++ src/screens/WishlistScreen.tsx
@@ -1,72 +1,85 @@
-import React, { useCallback } from 'react';
-import { View, Image, TextInput, TouchableOpacity } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import { View, Image, TextInput, TouchableOpacity, Text, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { LeftArrowIcon, SaveIcon, ShareIcon, StarIcon, ScheduleIcon, MarkerIcon, SearchArrowIcon, SearchingIcon } from '@/assets/icons';
+import { SearchArrowIcon, SearchingIcon } from '@/assets/icons';
+import BottomSheet from '@gorhom/bottom-sheet';
+import CustomBottomSheet from '@/components/ui/CustomBottomSheet';
 import { SearchContainer } from '@/components/ui';
+import { Chip} from '@/components/ui';
 import type { RootStackParamList } from '@/navigation/types';
-import MapView, {
- 
-  PROVIDER_GOOGLE,
-} from 'react-native-maps';
-import { IconButton } from '@/screens/destinationDetail/components';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
 // ============ Types ============
 type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
 
 // ============ Component ============
 const WishlistScreen: React.FC = () => {
   const navigation = useNavigation<NavigationProp>();
-
+  const bottomSheetRef = useRef<BottomSheet>(null);
   // 1. Hooks
   const handleGoBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
 
-  const handleSave = useCallback(() => {
-    // TODO: 저장 기능 구현
+  const handleExpand = useCallback(() => {
+    bottomSheetRef.current?.expand();
   }, []);
 
-  const handleShare = useCallback(() => {
-    // TODO: 공유 기능 구현
+  const handleCollapse = useCallback(() => {
+    bottomSheetRef.current?.collapse();
   }, []);
 
   // 2. 렌더링
   return (
     <SafeAreaView className="flex-1 bg-screenBackground" edges={['top']}>
       <View className="flex-1">
-      <MapView provider={PROVIDER_GOOGLE} style={{ flex: 1 }}initialRegion={{
-          latitude: 37.482476,
-          longitude:126.941574,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-            </MapView> 
-         
-                    <View  className="absolute top-[5px] left-4 right-4 z-10" >  
-                  <SearchContainer>
-                      {/* 왼쪽: 뒤로가기 버튼 */}
-      <TouchableOpacity  onPress={handleGoBack}>
-   <SearchArrowIcon/>
-  
-  </TouchableOpacity>
-  {/* 2. 검색 입력창 */}
-  <View className="">
-  <TextInput
-    className="flex-1  text-h3 font-regular pr-12"
-    placeholder="희망하는 관광지를 검색하세요"
-    placeholderTextColor="#999"
-  /></View>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={{ flex: 1 }}
+          //임시로 봉천동
+          initialRegion={{
+            latitude: 37.482476,
+            longitude: 126.941574,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}></MapView>
 
-  {/* 3. 검색 아이콘 (필요시) */}
-<TouchableOpacity className="absolute right-4">
-  <SearchingIcon/>
-  </TouchableOpacity>
-</SearchContainer>
-</View>
-</View>
-             
- 
+        <View className="absolute top-[5px] left-4 right-4 z-10">
+          <SearchContainer>
+            {/* 왼쪽: 뒤로가기 버튼 */}
+            <TouchableOpacity onPress={handleGoBack} className="ml-4 mr-2">
+              <SearchArrowIcon />
+            </TouchableOpacity>
+            {/* 2. 검색 입력창 */}
+
+            <TextInput
+              className="flex-1  text-h3 font-regular pr-12 text-black"
+              placeholder="희망하는 관광지를 검색하세요"
+              placeholderTextColor="#8C7B73"
+            />
+
+            {/* 3. 검색 아이콘 */}
+            <TouchableOpacity className="absolute right-4">
+              <SearchingIcon />
+            </TouchableOpacity>
+          </SearchContainer>
+        </View>
+        <CustomBottomSheet>
+          <View style={{ padding: 20 }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+            
+                      {/* 카테고리 Chip */}
+                      <View className="mt-6 px-4 flex-row">
+                        <Chip label="관광지" className="mr-2" />
+                        <Chip label="문화" className="mr-2" />
+                        <Chip label="역사" />
+                      </View>
+            </Text>
+          </View>
+        </CustomBottomSheet>
+      </View>
     </SafeAreaView>
   );
 };
