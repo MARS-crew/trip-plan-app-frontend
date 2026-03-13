1ca55e6df4980a611ea8da4e6ac8d0b5c1af7229
47mango <do_hh@naver.com>
2026-03-13 10:25:04 +0900

    feat : PLI-8 ( TopBar 컴포넌트 제작 + 알림 리스트 )

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -12,6 +12,7 @@ import {
   SavedScreen,
   MyPageScreen,
 } from '@/screens';
+import HomeStackNavigator from './HomeStackNavigator';
 import SearchStackNavigator from './SearchStackNavigator';
 import { HomeIcon, SearchIcon, MapIcon, BookmarkIcon, MyPageIcon } from '@/assets/icons';
 import { COLORS } from '@/constants';
@@ -193,7 +194,7 @@ const BottomTabNavigator: React.FC = () => {
       />
       <Tab.Screen
         name="Home"
-        component={HomeScreen}
+        component={HomeStackNavigator}
         options={{ tabBarButton: createTabBarButton('Home', '홈') }}
       />
       <Tab.Screen
