eb675ebd15f82a4445befc499743b5e7e15db07a
takjinwu <takjinwu0613@gmail.com>
2026-03-14 04:02:04 +0900

    오류 해결

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -4,6 +4,6 @@ export { default as SearchScreen } from './search';
 export { default as MyTripScreen } from './myTrip';
 export { default as SavedScreen } from './saved';
 export { default as MyPageScreen } from './myPage';
-export { default as AccountSettingsScreen } from './accountSettings';
+export { default as AccountSettingsScreen } from './accountSettings/AccountSettingsScreen';
 export { default as DestinationDetailScreen } from './destinationDetail';
 export { default as SelectTripScreen } from './selectTrip';
