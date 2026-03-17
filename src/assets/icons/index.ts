63a6f80816c342ae8c2dcf0918031860bc5d1b8d
최혜림 <101788177+hyerim03@users.noreply.github.com>
2026-03-17 09:15:25 +0900

    Revert "Feature pli 14"

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -39,10 +39,6 @@ export { default as VectorGrayIcon } from './vectorgray.svg';
 export { default as BackArrow } from './backArrow.svg';
 export { default as WeatherInfoIcon } from './weather-info.svg';
 export { default as ScheduleInfoIcon } from './schedule-info.svg';
-export { default as CalendarIcon } from './calendar.svg';
-export { default as ChevronDownIcon } from './chevrondown.svg';
-export { default as ChevronUpIcon } from './chevronup.svg';
-export { default as AirplaneIcon } from './airplane.svg';
 
 // 타입 정의
 export interface IconProps {
