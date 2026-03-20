e61903e11c9c05b009ad07c6e0ed756da22c3e79
47mango <do_hh@naver.com>
2026-03-20 19:55:25 +0900

    Revert "Merge pull request #46 from MARS-crew/feature-PLI-8"
    This reverts commit 7ab1bc4834f8b84d278daca291a2a2c21a699916, reversing
    changes made to 363fd977ad439e43922ec6051f48e8a9178ed29d.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -10,6 +10,7 @@ export { ContentContainer } from './ContentContainer';
 export type { ContentContainerProps } from './ContentContainer';
 export { TripStatusChip } from './TripStatusChip';
 export type { TripStatusChipProps, TripStatus } from './TripStatusChip';
+
 export { TopBar } from './TopBar'
 export type { TopBarProps } from './TopBar'
 
