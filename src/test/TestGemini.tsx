0e63f6ae760155ff51f0b98290e8c2807fb2e850
hyerim03 <chhari0708@naver.com>
2026-04-07 14:42:31 +0900

    feat: test code

---
+++ src/test/TestGemini.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native"
+
+const test = () => {
+  const data: any = null
+
+  return (
+    <View style={{ margin: 13 }}>
+      <Text>test</Text>
+    </View>
+  )
+}
