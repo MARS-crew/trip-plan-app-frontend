b3778f14e5e2855c23212c18da7f3272e7bf20e6
Kimbosung521 <sorisl37@naver.com>
2026-04-09 15:31:33 +0900

    fix: PLI-19(검색창 키보드 수정)

--- src/screens/wishList/components/WishlistSearchBar.tsx
+++ src/screens/wishList/components/WishlistSearchBar.tsx
@@ -8,30 +8,29 @@ import type { WishlistSearchBarProps } from '@/screens/wishList/types';
 export const WishlistSearchBar = React.memo<WishlistSearchBarProps>(
   ({ searchInputRef, searchQuery, onChangeText, onFocus, onBlur, onFocusInput, onPressBack }) => {
     return (
-      <View className="absolute left-4 right-4 top-[5px] z-50">
-        <SearchContainer onTouchStart={onFocusInput}>
-          <TouchableOpacity
-            onPress={onPressBack}
-            className="ml-4 mr-2"
-            accessibilityRole="button"
-            accessibilityLabel="검색 종료">
-            <SearchArrowIcon />
-          </TouchableOpacity>
-          <TextInput
-            ref={searchInputRef}
-            className="flex-1 pr-12 font-pretendardRegular text-h3 text-black"
-            placeholder="희망하는 관광지를 검색하세요"
-            placeholderTextColor={COLORS.gray}
-            value={searchQuery}
-            onChangeText={onChangeText}
-            onFocus={onFocus}
-            onBlur={onBlur}
-          />
-          <View className="absolute right-4" accessible={false} pointerEvents="none">
-            <SearchingIcon />
-          </View>
-        </SearchContainer>
-      </View>
+      <SearchContainer className="absolute left-4 right-4 top-[5px] z-50">
+        <TouchableOpacity
+          onPress={onPressBack}
+          className="ml-4 mr-2"
+          accessibilityRole="button"
+          accessibilityLabel="검색 종료">
+          <SearchArrowIcon />
+        </TouchableOpacity>
+        <TextInput
+          ref={searchInputRef}
+          className="flex-1 pr-12 font-pretendardRegular text-h3 text-black"
+          placeholder="희망하는 관광지를 검색하세요"
+          placeholderTextColor={COLORS.gray}
+          value={searchQuery}
+          onChangeText={onChangeText}
+          onPressIn={onFocusInput}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
+        <TouchableOpacity className="absolute right-4">
+          <SearchingIcon />
+        </TouchableOpacity>
+      </SearchContainer>
     );
   },
 );
